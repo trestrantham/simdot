b2895754dc9f2f6504df74d62828149fa027ae53
Tres Trantham <tres@trestrantham.com>
2013-03-15 11:36:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Simdot::Application.config.secret_key_base = '193de0afd6835374103b68d52d31bcc065e6955dfa3281d9649a1aa08fab9f6307d394480a2c3cbd3f29d837a4aa5127104d6ec499dfec57dd54c9335848b5f9'
