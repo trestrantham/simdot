86e0586737df43f70040f5ac9f8be016202c7bd8
Tres Trantham <tres@trestrantham.com>
2013-03-25 11:09:19 -0400

    Remove /test

--- test/helpers/frequencies_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class FrequenciesHelperTest < ActionView::TestCase
-end
--- test/models/interface_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class InterfaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
