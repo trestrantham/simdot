22463b883d77dd86e9c5b2657d9a560bf13447f5
Tres Trantham <tres@trestrantham.com>
2013-03-25 16:11:35 -0400

    Preparation for Heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Assets should be precompiled for production (so we don't need the gems loaded then)
-# Bundler.require(*Rails.groups(assets: %w(development test)))
+Bundler.require(*Rails.groups(assets: %w(development test)))
 
 module Simdot
   class Application < Rails::Application
@@ -18,5 +18,7 @@ module Simdot
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.initialize_on_precompile = false
   end
 end
