4e3ca8b3bf77a2990df2e7c978e7c5d17f08ace0
Tres Trantham <tres@trestrantham.com>
2013-04-06 13:13:50 -0400

    Scope a category to its context

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :context
 
-  validates :name,    presence: true, uniqueness: true
-  validates :context, presence: true
+  validates :name,       presence: true, uniqueness: { scope: :context_id }
+  validates :context_id, presence: true, uniqueness: { scope: :name }
 end
