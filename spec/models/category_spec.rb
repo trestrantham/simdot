f109f80b37688c310be934222ec980e7b30e28da
Tres Trantham <tres@trestrantham.com>
2013-04-08 16:09:31 -0400

    Add tests for Context and Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    create(:category).should be_valid
+  end
+
+  it "is invalid without a name" do
+    build(:category, name: nil).should_not be_valid
+  end
+
+  it "is invalid without a context" do
+    build(:category, context_id: nil).should_not be_valid
+  end
+
+  describe "#name" do
+    it "has a unique name for a given Context" do
+      category = create(:category)
+      build(:category, name: category.name, context: category.context).should_not be_valid
+    end
+
+    it "allows a duplicate name for a separate Context" do
+      context1 = create(:context, name: "Foo")
+      context2 = create(:context, name: "Bar")
+      category = create(:category, context: context1)
+      build(:category, name: category.name, context: context2).should be_valid
+    end
+  end
 end
--- spec/models/context_spec.rb
+++ spec/models/context_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Context do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    create(:context).should be_valid
+  end
+
+  it "is invalid without a name" do
+    build(:context, name: nil).should_not be_valid
+  end
+
+  it "has a unique name" do
+    context = create(:context)
+    build(:context, name: context.name).should_not be_valid
+  end
+
+  describe "#categories" do
+    it "responds to zero Categories" do
+      context = create(:context)
+      context.categories.should be_empty
+    end
+
+    it "responds to one or more Categories" do
+      context = create(:context)
+      category = create(:category, context: context)
+      context.categories.first.should eq(category)
+    end
+  end
 end
